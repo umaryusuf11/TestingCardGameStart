c0c2126eeae037218c152103d44c57794122abcb
Umar Farouk Yusuf <umaryusuf11@gmail.com>
2022-01-25 15:54:11 +0000

    + added more tests and refactored old test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -7,17 +7,45 @@ public class PlayerTest {
 
     @Test
     void hasWon() {
+        Player player = new Player(PlayerType.USER, "p1", 3);
         player.setWinner(true);
         assertTrue(player.hasWon());
     }
 
     @Test
-    void getPlayerType() {
+    void hasNotWon() {
+        Player player = new Player(PlayerType.USER, "p1", 3);
+        player.setWinner(false);
+        assertFalse(player.hasWon());
+    }
+
+    @Test
+    void getPlayerTypeUser() {
         assertEquals(PlayerType.USER, player.getPlayerType());
     }
 
     @Test
+    void getPlayerTypeComputer() {
+        Player player = new Player(PlayerType.COMPUTER, "p1", 3);
+        assertEquals(PlayerType.COMPUTER, player.getPlayerType());
+    }
+
+    @Test
+    void getPlayerTypeDealer() {
+        Player player = new Player(PlayerType.DEALER, "p1", 3);
+        assertEquals(PlayerType.DEALER, player.getPlayerType());
+    }
+
+    @Test
     void hasHand() {
+        Player player = new Player(PlayerType.USER, "p1", 3);
+        player.setHand(new Hand());
+        assertTrue(player.hasHand());
+    }
+
+    @Test
+    void notHasHand() {
+        Player player = new Player(PlayerType.USER, "p1", 3);
         assertFalse(player.hasHand());
     }
 
